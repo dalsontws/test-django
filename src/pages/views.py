6fa5d270831fca929fd2a34606af85ec11365eed
dalsontws <dalsontws@gmail.com>
2019-12-12 11:16:40 +0800

    complete html tags

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,9 +5,24 @@ from django.shortcuts import render
 
 
 def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
     # return HttpResponse("<h1>Hello World<h1/>")
     return render(request, "home.html", {})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page<h1/>")
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [123, 234, 345]
+    }
+    return render(request, "about.html", my_context)
+
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page<h1/>")
